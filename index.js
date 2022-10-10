e80d699f0ed5475746bb85f4e30b7b6f7d2b2c7b
thomas santos <thomascsantos2@gmail.com>
2022-10-10 14:48:25 -0300

    localStorage

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 let titulo = document.getElementById("titulo")
 
+titulo.innerHTML = localStorage.getItem("titulo") || "0"
+
 let adiciona = document.getElementById("adiciona").addEventListener("click", () => {
     titulo.innerHTML = parseInt(titulo.innerHTML) + 1
+    memory()
 })
 let subtrai = document.getElementById("subtrai").addEventListener("click", () => {
     titulo.innerHTML = parseInt(titulo.innerHTML) - 1
+    memory()
 })
 let zera = document.getElementById("zera").addEventListener("click", () => {
     titulo.innerHTML = titulo.innerHTML = 0
-
+    memory()
 })
+function memory() {
+    localStorage.setItem("titulo", titulo.innerHTML)
+}
+memory()
